74b0dfc73cf1685d000fc69ed2742628ee10b757
Fran <fran.p.21@gmail.com>
2021-04-19 11:04:31 -0300

    Agrega msw para testear el cliente mockeando la API

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,2 +1,18 @@
 // src/setupTests.js
 import "@testing-library/jest-dom";
+import { server } from "./mocks/server";
+// This configures a request mocking server with the given request handlers.
+beforeAll(() => {
+  // Enable the mocking in tests.
+  server.listen({ onUnhandledRequest: "error" });
+});
+
+afterEach(() => {
+  // Reset any runtime handlers tests may use.
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  // Clean up once the tests are done.
+  server.close();
+});
